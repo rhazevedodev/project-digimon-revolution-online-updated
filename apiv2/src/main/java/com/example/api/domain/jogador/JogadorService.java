1fd97cb1396d363bb002ee601a1b84738d550f23
Rafael Azevedo <rafael.azevedo.dev@gmail.com>
2024-08-30 00:45:10 -0300

    + ADD dependencia do spring security + ADD configuracao necessaria para conseguir fazer as requisicoes agora que o spring security foi ativado + ADD criptografia em cima da senha que está sendo cadastrada

--- apiv2/src/main/java/com/example/api/domain/jogador/JogadorService.java
+++ apiv2/src/main/java/com/example/api/domain/jogador/JogadorService.java
@@ -2,6 +2,7 @@ package com.example.api.domain.jogador;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +12,11 @@ public class JogadorService {
     private static final Logger logger = LoggerFactory.getLogger(JogadorService.class);
 
     private final JogadorRepository jogadorRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public JogadorService(JogadorRepository jogadorRepository) {
+    public JogadorService(JogadorRepository jogadorRepository, PasswordEncoder passwordEncoder) {
         this.jogadorRepository = jogadorRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
@@ -24,6 +27,9 @@ public class JogadorService {
             logger.warn("Tentativa de cadastro com email já em uso: {}", jogador.getEmail());
             throw new RuntimeException("Email já está em uso");
         }
+        logger.info("Criptografando a senha do jogador antes de salvar", jogador);
+        String senhaCriptografada = passwordEncoder.encode(jogador.getSenha());
+        jogador.setSenha(senhaCriptografada);
         return jogadorRepository.save(jogador);
     }
 
