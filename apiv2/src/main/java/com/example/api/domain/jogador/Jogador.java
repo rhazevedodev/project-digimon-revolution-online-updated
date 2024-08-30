369a7aea486e54ae96e919f3cb82e75a3d323fb0
Rafael Azevedo <rafael.azevedo.dev@gmail.com>
2024-08-30 00:31:53 -0300

    + ADD adicionados 2 novos campos na classe de Jogador

--- apiv2/src/main/java/com/example/api/domain/jogador/Jogador.java
+++ apiv2/src/main/java/com/example/api/domain/jogador/Jogador.java
@@ -7,6 +7,10 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -26,6 +30,10 @@ public class Jogador {
     private String email;
     @NotBlank(message = "A senha é obrigatória")
     private String senha;
+    @CreatedBy
+    private String criadoPor;
+    @CreatedDate
+    private LocalDateTime criadoEm = LocalDateTime.now();
 
     public Jogador(String nome, String email, String senha) {
         this.nome = nome;
