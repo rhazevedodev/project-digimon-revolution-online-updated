27b7058c8c652f79fe89c714d6be02eaf8ae53af
Rafael Azevedo <rafael.azevedo.dev@gmail.com>
2024-08-30 01:06:28 -0300

    + ADD adicionado arquivo listando os cenarios de teste da funcionalidade CadastrarJogador + ADD novos parametros de validacao dos campos do modelo Jogador

--- apiv2/src/main/java/com/example/api/domain/jogador/Jogador.java
+++ apiv2/src/main/java/com/example/api/domain/jogador/Jogador.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 
@@ -24,11 +25,14 @@ public class Jogador {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "O nome é obrigatório")
+    @Length(min = 4, max = 20, message = "O nome deve ter entre 4 e 20 caracteres")
     private String nome;
     @NotBlank(message = "O email é obrigatório")
     @Email(message = "O email deve ser válido")
+    @Length(max = 50, message = "O email deve ter no máximo 50 caracteres")
     private String email;
     @NotBlank(message = "A senha é obrigatória")
+    @Length(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
     private String senha;
     @CreatedBy
     private String criadoPor;
